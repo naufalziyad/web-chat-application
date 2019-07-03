7bd3ea3a1b2a26baa82b5e9a85c3394a6f99eefa
Naufal Ziyad <naufal.ziyad@detik.com>
2019-07-03 15:21:49 +0700

    add log out & image profile

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 	"text/template"
 
 	"./trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -23,6 +28,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("public", t.filename)))
 	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
 	t.templ.Execute(w, r)
 
 }
@@ -31,9 +43,32 @@ func main() {
 	fmt.Println("---Web Chat Application---")
 	var addr = flag.String("addr", ":8999", "The address of the application.")
 	flag.Parse()
+	//setup gomniauth
+	gomniauth.SetSecurityKey("key")
+	gomniauth.WithProviders(
+		facebook.New("key", "key",
+			URLDomain+"/auth/callback/facebook"),
+		github.New("key", "",
+			URLDomain+"/auth/callback/github"),
+		google.New(googleClientID, googleClientSecret,
+			URLDomain+"/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	http.Handle("/room", r)
 	go r.run()
 
